740333090059967ca4f1469edaf0eed3ab80d471
Omer Tanveer <omertanveer135@gmail.com>
2024-09-24 01:13:01 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Weather from './components/Weather'
+
+function App() {
+  return (
+    <div className='app'>
+      <h1 className='hd1'>Omer's Weather App</h1>
+      <Weather/>
+    </div>
+    
+  )
+}
+
+export default App
